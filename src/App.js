869e9acb1f988acb71e2eeecf1a35c6fbd3ba494
Irene R <irene@smallwondermedia.com>
2019-04-08 22:40:58 -0400

    icon name but no icon

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import axios from 'axios';
 import Current from "./components/Current/Current";
 import Hourly from "./components/Hourly/Hourly";
 import Daily from "./components/Daily/Daily";
-import "./icons/clear-day.svg";
-import "./icons/clear-night.svg";
-import "./icons/cloudy.svg";
-import "./icons/fog.svg";
-import "./icons/partly-cloudy-day.svg";
-import "./icons/partly-cloudy-night.svg";
-import "./icons/rain.svg";
-import "./icons/sleet.svg";
-import "./icons/snow.svg";
-import "./icons/wind.svg";
+// import "./icons/clear-day.svg";
+// import "./icons/clear-night.svg";
+// import "./icons/cloudy.svg";
+// import "./icons/fog.svg";
+// import "./icons/partly-cloudy-day.svg";
+// import "./icons/partly-cloudy-night.svg";
+// import "./icons/rain.svg";
+// import "./icons/sleet.svg";
+// import "./icons/snow.svg";
+// import "./icons/wind.svg";
 
 class App extends Component {
 
@@ -25,7 +25,7 @@ class App extends Component {
         dailyWeather: [],
         dailyTomorrow: [],
         dailyDayAfter: [],
-        // currentIcon: "".replace(/\s/g,'-')
+        currentIcon: null
     }
 
     componentDidMount() {
@@ -53,12 +53,18 @@ class App extends Component {
             console.log(result);
             this.setState({
                 currentWeather: result.currently,
-                // currentIcon: result.currently.icon,
+                // currentIcon: result.currently.icon.replace(/\s/g,'-'),
                 hourlyWeather: result.hourly,
                 dailyWeather: result.daily,
                 dailyTomorrow: result.daily.data[0],
                 dailyDayAfter: result.daily.data[1]
             });
+            const icon = this.state.currentWeather.icon.replace(/\s/g, '');
+            const iconSVG = icon.svg;
+            this.setState({
+                currentIcon: iconSVG
+            });
+            
         })
     }
 
@@ -74,7 +80,7 @@ class App extends Component {
             summary={this.state.currentWeather.summary}
             temp={this.state.currentWeather.temperature}
             feels={this.state.currentWeather.apparentTemperature}
-            icon={this.state.currentWeather.icon}
+            icon={`${this.state.currentWeather.icon}.svg`}
         />
 
         <br/>
--- src/components/Current/Current.js
+++ src/components/Current/Current.js
@@ -1,5 +1,14 @@
 import React from 'react';
-
+import "./icons/clear-day.svg";
+import "./icons/clear-night.svg";
+import "./icons/cloudy.svg";
+import "./icons/fog.svg";
+import "./icons/partly-cloudy-day.svg";
+import "./icons/partly-cloudy-night.svg";
+import "./icons/rain.svg";
+import "./icons/sleet.svg";
+import "./icons/snow.svg";
+import "./icons/wind.svg";
 
 
 const Current = (props) => {
@@ -15,7 +24,7 @@ const Current = (props) => {
             <br/>
             <strong>Feels like:</strong> {parseInt(props.feels)}° F
             <br/>
-            <img src={`${props.icon}.svg`} alt={props.icon}/> 
+            <img src={props.icon} alt={props.icon}/> 
         </div>
 
     )
