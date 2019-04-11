97e78b498ebc11bbebba0d879dae2803b07cee17
Irene R <irene@smallwondermedia.com>
2019-04-10 21:34:28 -0400

    backup

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
         dailyWeather: [],
         dailyTomorrow: [],
         dailyDayAfter: [],
-        currentIcon: null
+        currentIcon: ""
     }
 
     componentDidMount() {
