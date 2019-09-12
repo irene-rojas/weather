08aa2d8dc91338585dd7ccfce60999f796a28206
Irene Rojas <irene@smallwondermedia.com>
2019-09-11 21:14:43 -0400

    Updates

---
+++ precache-manifest.622dca1e9f7f0260368321bfed04c8ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b3f983c2c758fdf2f6d",
+    "url": "/weather/static/css/main.1e9530da.chunk.css"
+  },
+  {
+    "revision": "7b3f983c2c758fdf2f6d",
+    "url": "/weather/static/js/main.600d16c6.chunk.js"
+  },
+  {
+    "revision": "3cb3cde5c978189545b4",
+    "url": "/weather/static/js/runtime~main.6cff1e35.js"
+  },
+  {
+    "revision": "2568bda9e04f075ae0ab",
+    "url": "/weather/static/js/2.244cccb9.chunk.js"
+  },
+  {
+    "revision": "bea2d77de2f1af5a98cc4d7c3b07f0c5",
+    "url": "/weather/static/media/spinning.bea2d77d.gif"
+  },
+  {
+    "revision": "7fb755701df05510a7f1c81e32081ef0",
+    "url": "/weather/static/media/map.7fb75570.png"
+  },
+  {
+    "revision": "6b7912a97a572a019e1fcf5c18893c6b",
+    "url": "/weather/index.html"
+  }
+];
